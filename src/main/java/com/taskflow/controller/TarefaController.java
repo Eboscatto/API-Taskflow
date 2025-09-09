9219fbb18816a910fecd2395131eb676c11d8730
Eboscaatto <e_boscatto@hotmail.com>
2025-09-09 20:48:18 -0300

    Implementados métodos

--- src/main/java/com/taskflow/controller/TarefaController.java
+++ src/main/java/com/taskflow/controller/TarefaController.java
@@ -1,5 +1,41 @@
 package com.taskflow.controller;
 
+import com.taskflow.model.Tarefa;
+import com.taskflow.service.TarefaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tarefas")
+@CrossOrigin(origins = "http://localhost:8080") // Porta 3000 libera o acesso do React
 public class TarefaController {
+
+    private final TarefaService service;
+
+    public TarefaController(TarefaService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Tarefa> listar(){
+        return service.listar();
+    }
+    @PostMapping
+    public Tarefa criar(@RequestBody Tarefa tarefa) {
+        return service.salvar(tarefa);
+    }
+
+    @PutMapping("/{id}")
+    public Tarefa atualizar(@PathVariable Long id, @RequestBody Tarefa tarefa) {
+        tarefa.setId(id);
+        return service.salvar(tarefa);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
+
 
 }
--- src/main/java/com/taskflow/service/TarefaService.java
+++ src/main/java/com/taskflow/service/TarefaService.java
@@ -1,58 +1,38 @@
 package com.taskflow.service;
 
+import com.taskflow.model.Tarefa;
+import com.taskflow.repository.TarefaRepository;
 import jakarta.persistence.*;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
 
-@Entity
+@Service
 public class TarefaService {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String titulo;
-    private String descricao;
-    private boolean concluida = false;
-    private LocalDate prazo;
 
-    // Getters e Setters
+    private final TarefaRepository repository;
 
-    public Long getId() {
-        return id;
+    public TarefaService(TarefaRepository repository) {
+        this.repository = repository;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public List<Tarefa> listar() {
+        return repository.findAll();
     }
 
-    public String getTitulo() {
-        return titulo;
+    public Optional<Tarefa> buscar(Long id) {
+        return repository.findById(id);
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    public Tarefa salvar(Tarefa tarefa) {
+        return repository.save(tarefa);
     }
 
-    public String getDescricao() {
-        return descricao;
+    public void deletar(Long id) {
+        repository.deleteById(id);
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public boolean isConcluida() {
-        return concluida;
-    }
-
-    public void setConcluida(boolean concluida) {
-        this.concluida = concluida;
-    }
-
-    public LocalDate getPrazo() {
-        return prazo;
-    }
-
-    public void setPrazo(LocalDate prazo) {
-        this.prazo = prazo;
-    }
 }
